66e2bd9db967eb3126bfcd3c617be4dffe0de700
Flavia Ballabene <139667150+FlaviaBallabene@users.noreply.github.com>
2024-02-27 05:21:58 +0000

    navbarandintro

---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+//create your first component
+const Navbar = () => {
+  return (
+    <div className="text-center">
+      <nav
+        className="navbar navbar-expand-lg bg-dark border-bottom border-body"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <div className="row w-100 justify-content-between">
+            <div className="col-auto">
+              <a className="navbar-brand" href="#">
+                Start Bootstrap
+              </a>
+            </div>
+            <div className="col-auto">
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+            </div>
+          </div>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+import "./styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Navbar from "./component/navbar.jsx";
+import Intro from "./component/intro.jsx";
+import Cards from "./component/cards.jsx";
+import Footer from "./component/footer.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Navbar />, document.querySelector("#navbar"));
+ReactDOM.render(<Intro />, document.querySelector("#intro"));
+ReactDOM.render(<Cards />, document.querySelector("#cards"));
+ReactDOM.render(<Footer />, document.querySelector("#footer"));
