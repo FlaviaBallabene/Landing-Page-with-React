34064b877f641b821d970cecd3ce08ff48bee681
stanxlin <143833064+stanxlin@users.noreply.github.com>
2024-02-28 02:11:43 +0000

    card title and footer complete!

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,17 +1,64 @@
 import React from "react";
 
-//create your first component
 const Cards = () => {
-	return (
-	  <div className="card" style={{ width: "18rem" }}>
-		<img src="" className="card-img-top" alt="placeholder" />
-		<div className="card-body">
-		  <h5 className="card-title">Card title</h5>
-		  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		  <a href="#" className="btn btn-primary">Go somewhere</a>
-		</div>
-	  </div>
-	);
-  };
-  
+  return (
+    <div className="row">
+      {/* First Card */}
+      <div className="col-md-3">
+        <div className="card mx-auto text-center mb-3" style={{ width: "18rem" }}>
+          <div className="card-img-top" style={{ backgroundColor: "#f0f0f0", height: "200px", border: "1px solid #ccc" }}>
+            {/* This is the blank box */}
+          </div>
+          <div className="card-body">
+            <h5 className="card-title text-center">Card title </h5>
+            <p className="card-text text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex quam sit totam pariatur ...</p>
+            <a href="#" className="btn btn-primary d-block mx-auto">Found out more!</a>
+          </div>
+        </div>
+      </div>
+
+      {/* Second Card */}
+      <div className="col-md-3">
+        <div className="card mx-auto text-center mb-3" style={{ width: "18rem" }}>
+          <div className="card-img-top" style={{ backgroundColor: "#f0f0f0", height: "200px", border: "1px solid #ccc" }}>
+            {/* This is the blank box */}
+          </div>
+          <div className="card-body">
+            <h5 className="card-title text-center">Card title </h5>
+            <p className="card-text text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex quam sit totam pariatur ...</p>
+            <a href="#" className="btn btn-primary d-block mx-auto">Found out more!</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="col-md-3">
+        <div className="card mx-auto text-center mb-3" style={{ width: "18rem" }}>
+          <div className="card-img-top" style={{ backgroundColor: "#f0f0f0", height: "200px", border: "1px solid #ccc" }}>
+            {/* This is the blank box */}
+          </div>
+          <div className="card-body">
+            <h5 className="card-title text-center">Card title </h5>
+            <p className="card-text text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex quam sit totam pariatur ...</p>
+            <a href="#" className="btn btn-primary d-block mx-auto">Found out more!</a>
+          </div>
+        </div>
+      </div>
+
+	  <div className="col-md-3">
+        <div className="card mx-auto text-center mb-3" style={{ width: "18rem" }}>
+          <div className="card-img-top" style={{ backgroundColor: "#f0f0f0", height: "200px", border: "1px solid #ccc" }}>
+            {/* This is the blank box */}
+          </div>
+          <div className="card-body">
+            <h5 className="card-title text-center">Card title </h5>
+            <p className="card-text text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex quam sit totam pariatur ...</p>
+            <a href="#" className="btn btn-primary d-block mx-auto">Found out more!</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Cards;
+
--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: "lightgray", height: "50px" }}>
+    <footer style={{ backgroundColor: "black", height: "50px", position: "fixed", bottom: "0", width: "100%", zIndex: "1000" }}>
       {/* Empty div for the line */}
       <div style={{ borderBottom: "1px solid gray" }}></div>
     </footer>
