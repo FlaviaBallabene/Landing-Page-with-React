3470adfacffc9e1ecfe5462f33f398a7311e3711
stanxlin <143833064+stanxlin@users.noreply.github.com>
2024-02-28 01:24:35 +0000

    cards.jsx test code

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -3,21 +3,15 @@ import React from "react";
 //create your first component
 const Cards = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  <div className="card" style={{ width: "18rem" }}>
+		<img src="" className="card-img-top" alt="placeholder" />
+		<div className="card-body">
+		  <h5 className="card-title">Card title</h5>
+		  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  <a href="#" className="btn btn-primary">Go somewhere</a>
 		</div>
+	  </div>
 	);
-};
-
+  };
+  
 export default Cards;
